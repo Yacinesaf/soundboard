0edd8662d9472ea0c9ffdd561d9a4095893ecc5c
Yacinesaf <yacinesaf96@gmail.com>
2020-08-04 15:28:20 -0400

    moved object to app

--- src/PianoTile.js
+++ src/PianoTile.js
@@ -1,30 +1,24 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Button } from '@material-ui/core';
-import sound1 from './assets/sound1.wav'
 
 
-function PianoTile({ tileName, audio }) {
+
+function PianoTile({ tileName, sound }) {
   function play() {
-    var audio = document.getElementById("audio");
-    audio.play();
+    var audios = document.getElementById("audio");
+    audios.play();
   }
 
-  const obj = {
-    sound1 : sound1
-  }
+
   return (
-    // <div>
-    //   <audio controls id='audio' >
-    //     <source type='audio/wav' src='./assets/sound1.wav' />
-    //   </audio>
-    //   <Button onClick={play} style={{ padding: 15, border: '2px solid grey', borderRadius: 5 }}>
-    //     {tileName}
-    //   </Button>
-    // </div>
-    <audio controls>
-      <source src={obj['sound1']} type="audio/wav" />
-          Your browser does not support the audio element.
-    </audio>
+    <div>
+      <audio id='audio' >
+        <source type='audio/wav' src={sound} />
+      </audio>
+      <Button onClick={play} style={{ padding: 15, border: '2px solid grey', borderRadius: 5 }}>
+        {tileName}
+      </Button>
+    </div>
 
   );
 }
